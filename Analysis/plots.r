418a79b8a5ef57b8e259343a094ebd72e102cb7d
clint-leach <clint.leach@gmail.com>
2017-04-11 16:03:25 -0600

    Adding figures 2 and 3.

--- Analysis/plots.r
+++ Analysis/plots.r
@@ -19,7 +19,7 @@ post <- ddply(tseries, .(tot.week), summarise,
 sim <- readRDS("Results/mcmc.rds")
 
 #===============================================================================
-# Figure 1
+# Figure 1: Observed and estimated time series
 
 # Computing posterior median and 80% credible interval
 yhat <- rstan::extract(sim, c("y_hat"), permute = F) %>% apply(3, quantile, c(0.1, 0.5, 0.9))
@@ -65,20 +65,64 @@ grid.arrange(fig1.a, fig1.b, ncol = 2)
 dev.off()
 
 #===============================================================================
-# Figure 2
+# Figure 2: weekly posterior predictive p-value for replicate mosquito
+# surveillance data
 
+# Extracting full MCMC chain of replicate data
 mcmc <- rstan::extract(sim, "q_hat", permute = T)[[1]]
 nmcmc <- dim(mcmc)[1]
 
+# Computing P(q^{rep}_t > q_t)
 for(i in 1:243){
   post$p[i] <- sum(mcmc[, i] > post$qobs[i]) / nmcmc
 }
 
+# Plot
+postscript("Manuscript/figures/fig2.eps",
+           width = 4, height = 3,
+           family = "ArialMT")
+
 ggplot(post, aes(tot.week, p)) +
   geom_line() + 
   theme_classic() + 
+  scale_y_continuous(expand = c(0, 0.01)) + 
+  scale_x_continuous(expand = c(0, 1)) +
   xlab("week") + 
   ylab("p")
 
+dev.off()
 #===============================================================================
-# Figure 3
+# Figure 3: observed and posterior autocorrelation functions
+
+# Calculating the observed autocorrelation function
+obsacf <- acf(post$qobs, lag.max = 55, plot = F)$acf
+
+# Calculating the posterior acf function
+acfs <- apply(mcmc, 1, function(x){acf(x, lag.max = 55, plot = F)$acf})
+
+# Median and 80% credible interval
+acfrange <- apply(acfs, 1, quantile, c(0.1, 0.5, 0.9))
+
+# Assembling data frame for ggplot
+acfdf <- data.frame("lag" = c(0:55), 
+                    "obs" = obsacf, 
+                    "med" = acfrange[2, ], 
+                    "min" = acfrange[1, ], 
+                    "max" = acfrange[3, ])
+
+# Plot
+postscript("Manuscript/figures/fig3.eps",
+           width = 4, height = 3,
+           family = "ArialMT")
+
+ggplot(acfdf, aes(lag, obs)) + 
+  geom_hline(yintercept = 0, color = "gray50") +
+  geom_linerange(aes(lag, ymin = min, ymax = max)) +
+  geom_point() +
+  theme_classic() +
+  xlab("lag (weeks)") +
+  ylab("autocorrelation") + 
+  scale_x_continuous(expand = c(0, 0.1))
+
+dev.off()
+
